3dfd964426f129550fb18fa809d23094119c3c13
caolanhamilton <caolanhamilton75@gmail.com>
2022-06-13 14:38:44 +0100

    Create responseive AllArticles page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import { AllArticles } from './Components/AllArticles';
+import { Header } from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-  
+      <Header></Header>
+      <AllArticles></AllArticles>
     </div>
   );
 }
