269a86ea84c678e1d2898e20dc3a52dce29465e8
caolanhamilton <caolanhamilton75@gmail.com>
2022-06-14 10:35:20 +0100

    Added error handling for endpoints that do not exist

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ArticlesByTopic } from './Components/ArticlesByTopic';
 import { TopicPicker } from './Components/TopicPicker';
 import { SingleArticle } from './Components/SingleArticle';
+import { Error } from './Components/Error';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Header className="mainHeader" />
         <TopicPicker />
         <Routes>
+          <Route path="*" element={<Error />} />
           <Route path="/" element={<AllArticles />} />
           <Route path="/topics/:topic" element={<ArticlesByTopic />} />
           <Route path="/articles/:articleID" element={<SingleArticle />} />
