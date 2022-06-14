bacdd9501d664cd39613b659024e9b512aa9880d
caolanhamilton <caolanhamilton75@gmail.com>
2022-06-14 12:19:01 +0100

    Allow down or up vote after opposite option is pressed

--- src/Components/Voter.jsx
+++ src/Components/Voter.jsx
@@ -2,23 +2,26 @@ import { patchArticleVotesById } from "../Utils/ApiCalls";
 import { useState } from "react";
 
 export const Voter = ({ articleID, setVote, setArticleVotingError }) => {
-  const [disableVoteBtn, setDisableVoteBtn] = useState(false);
+    const [disableUpVoteBtn, setDisableUpVoteBtn] = useState(false);
+     const [disableDownVoteBtn, setDisableDownVoteBtn] = useState(false);
 
   const handleUpVoteClick = () => {
     setVote((currVote) => currVote + 1);
-    setDisableVoteBtn(true);
+      setDisableUpVoteBtn(true);
+      setDisableDownVoteBtn(false)
     patchArticleVotesById(articleID, 1).catch((err) => {
       setArticleVotingError(true);
-      setDisableVoteBtn(false);
+      setDisableUpVoteBtn(false);
       setVote((currCount) => currCount - 1);
     });
   };
 
   const handleDownVoteClick = () => {
     setVote((currVote) => currVote - 1);
-    setDisableVoteBtn(true);
+      setDisableDownVoteBtn(true);
+      setDisableUpVoteBtn(false);
     patchArticleVotesById(articleID, -1).catch((err) => {
-      setDisableVoteBtn(false);
+      setDisableDownVoteBtn(false);
       setArticleVotingError(true);
       setVote((currCount) => currCount + 1);
     });
@@ -29,14 +32,14 @@ export const Voter = ({ articleID, setVote, setArticleVotingError }) => {
       <button
         className="articleVoteBtn"
         onClick={handleUpVoteClick}
-        disabled={disableVoteBtn}
+        disabled={disableUpVoteBtn}
       >
         Up vote
       </button>
       <button
         className="articleVoteBtn"
         onClick={handleDownVoteClick}
-        disabled={disableVoteBtn}
+        disabled={disableDownVoteBtn}
       >
         Down vote
       </button>
