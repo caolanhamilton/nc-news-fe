0ec84e2d4376e0b40d1e51fdfb6e18e3be59a837
caolanhamilton <caolanhamilton75@gmail.com>
2022-06-15 16:13:52 +0100

    Add return to home link on error component

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
+
 export const Error = ({ error }) => { 
     return (
       <div className="error">
         <h1>We have a problem 😥</h1>
         {error && <h2>Sorry, {error.message.toLowerCase()}.</h2>}
         {!error && <h2>Sorry, path does not exist.</h2>}
+        <Link to="/"><h3>Click here to return home!</h3></Link>
       </div>
     );
 }
